21d4a8413280349e601668bc11661ce1af583845
Eric Hardy <eric.hardy.29@gmail.com>
2024-01-27 18:56:51 +0100

    in progress

--- ovenTemperture/src/main.cpp
+++ ovenTemperture/src/main.cpp
@@ -1,29 +1,8 @@
 #include <Arduino.h>
 
-// From "SdFat - Adafruit Fork by Bill Greiman"
 #include <SPI.h>
-/*
-#include "SdFat.h"
-#include "sdios.h"
-*/
 #include <SD.h>
 File myFile;
-/*
-// SD card chip select pin
-const uint8_t chipSelect = SS;
-
-// file system
-SdFat sd;
-
-// print stream
-ArduinoOutStream cout(Serial);
-//------------------------------------------------------------------------------
-// store error strings in flash memory
-#define error(s) sd.errorHalt(F(s))
-//------------------------------------------------------------------------------
-
-// END From "SdFat - Adafruit Fork by Bill Greiman"...
-*/
 
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <MCUFRIEND_kbv.h>   // Hardware-specific library
@@ -40,6 +19,8 @@ MCUFRIEND_kbv tft;
 #define GREEN   0x07E0
 #define WHITE   0xFFFF
 #define GREY    0x8410
+
+int i = 2;
 
 void showmsgXY(int x, int y, int sz, const GFXfont *f, const char *msg)
 {
@@ -82,19 +63,11 @@ void demoTFT() {
 
 // SD CARD
 void testSD() {
-  Serial.print("Initializing SD card...");
-  if (!SD.begin()) {
-    Serial.println("initialization failed!");
-    return;
-  }
-  Serial.println("initialization done.");
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one before opening another.
   myFile = SD.open("test.txt", FILE_WRITE);
   // if the file opened okay, write to it:
   if (myFile) {
     Serial.print("Writing to test.txt...");
-    myFile.println("testing 1, 2, 3.");
+    myFile.println("3333 testing 1, 2, 3.");
     // close the file:
     myFile.close();
     Serial.println("done.");
@@ -122,21 +95,36 @@ void testSD() {
 
 void setup(void)
 {
-    Serial.begin(9600);
-    uint16_t ID = tft.readID();
-    Serial.println("Example: Font_simple");
-    Serial.print("found ID = 0x");
-    Serial.println(ID, HEX);
-    if (ID == 0xD3D3) ID = 0x9481; //force ID if write-only display
-    tft.begin(ID);
-    tft.setRotation(0);
+  Serial.begin(9600);
+  uint16_t ID = tft.readID();
+  Serial.println("Example: Font_simple");
+  Serial.print("found ID = 0x");
+  Serial.println(ID, HEX);
+  if (ID == 0xD3D3) ID = 0x9481; //force ID if write-only display
+  tft.begin(ID);
+  tft.setRotation(0);
+
+  Serial.print("Initializing SD card...");
+  if (!SD.begin()) {
+    Serial.println("initialization failed!");
+  } else {
+    Serial.println("initialization done.");
+    // open the file. note that only one file can be open at a time,
+    // so you have to close this one before opening another.
+    SD.remove("test.txt");
+    delay(500);
+    // myFile.close();
+  }
 }
 
 void loop(void)
 {
-    // demoTFT();
-    testSD();
-    delay(10000);
+  if (i == 0) {
+    demoTFT();
+  }
+  // i++;
+  // demoTFT();
+  testSD();
+  delay(5000);
 }
-
 
